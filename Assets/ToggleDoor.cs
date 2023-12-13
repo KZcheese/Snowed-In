65bcfa136997be892814c4ccdb805b9eb311e14d
gavin-epstein <wreakjoy@gmail.com>
2023-12-13 16:22:11 -0500

    door anims, generator topper and fusebox locations fixed

---
+++ Assets/ToggleDoor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDoor : MonoBehaviour
+{
+
+    public Animator animator;
+    private bool open = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = gameObject.GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       // gameObject.GetComponent<ToggleDoor>().toggle();
+    }
+    void toggle()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+        {
+      
+            if (!open)
+            {
+                animator.Play("door open",0,0.0f);
+                open = true;
+
+            }
+            else
+            {
+                animator.Play("door close",0,0.0f);
+                open = false;
+            }
+
+        }
+    }
+}
