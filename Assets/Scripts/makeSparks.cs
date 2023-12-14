2bb2ce4e424b1ecca775386eb320c9c550f8fffe
Kevin Zhan <kevin.w.zhan@gmail.com>
2023-12-14 17:21:50 -0500

    spatial audio + sparks sound

--- Assets/Scripts/makeSparks.cs
+++ Assets/Scripts/makeSparks.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class makeSparks : Clickable
 {
+    public AudioClip soundClip;
+    private AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+    
     public override void onClick()
     {
+        
         ParticleSystem sparks = gameObject.GetComponentInChildren<ParticleSystem>();
         if (!sparks.isPlaying || sparks.time > .2f)
         {
+            // Play locked sound if the door is locked
+            if (soundClip != null)
+            {
+                audioSource.PlayOneShot(soundClip);
+            }
             sparks.time = 0.0f;
             gameObject.GetComponentInChildren<ParticleSystem>().Play();
         }
